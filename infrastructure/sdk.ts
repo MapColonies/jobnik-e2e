9a653b38d3f279b752741a421ff3a95cff7d07f4
netanelC <netanel153@gmail.com>
2025-11-30 14:49:48 +0200

    feat: add registry

--- infrastructure/sdk.ts
+++ infrastructure/sdk.ts
@@ -1,9 +1,12 @@
 import { JobnikSDK, createApiClient } from "@map-colonies/jobnik-sdk";
+import { Registry } from "prom-client";
+
 const jobnikManagerUrl =
   process.env.JOBNIK_MANAGER_BASE_URL || `http://localhost:8080`;
 
 export function createJobnikSDKInstance(): JobnikSDK {
   const jobnikSDK = new JobnikSDK({
+    metricsRegistry: new Registry(),
     baseUrl: jobnikManagerUrl,
     httpClientOptions: {
       agentOptions: {},
